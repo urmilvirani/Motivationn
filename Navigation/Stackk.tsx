df8b9bc1cf44df7a415a0153ebd4a36b738b9d5e
urmilvirani <urmilvirani15@gmail.com>
2024-03-04 17:36:14 +0530

    back to normal

--- Navigation/Stackk.tsx
+++ Navigation/Stackk.tsx
@@ -43,7 +43,6 @@ const Stackk = () => {
     return (
 
         <Stack.Navigator>
-
             <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
             <Stack.Screen name="Signin" component={Signin} options={{ headerShown: false }} />
             <Stack.Screen name="Forgot" component={Forgot} options={{ headerShown: false }} />
